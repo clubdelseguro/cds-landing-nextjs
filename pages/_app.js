8261306aa1dc4cf85f6372b9c288d544188b92d8
Matias Moreno <maatiigmm@gmail.com>
2022-10-25 14:14:51 -0300

    Loader de next page

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="./assets/img-logo.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#FC8100" />
+        <meta name="description" content="Club Del Seguro" />
+        <meta property="og:image" content="https://clubdelseguro.cl/assets/Logo-CDS-Original.svg" />
+        <link rel="apple-touch-icon" href="./assets/img-logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
