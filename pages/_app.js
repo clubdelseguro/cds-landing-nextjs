1603f9c7cd08fe3e1048e464512ba25956295371
Matias Moreno <maatiigmm@gmail.com>
2022-12-23 15:49:33 -0300

    Se cambia tag a google manager

--- pages/_app.js
+++ pages/_app.js
@@ -20,28 +20,30 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107921532-1"></script>
+
         <script
           dangerouslySetInnerHTML={{
-            __html: `(window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-107921532-1'););`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WJWJWCS');`,
           }}
         />
       </Head>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZDD5FJ"
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJWJWCS"
         height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <div className="contenido">
-            <NextNProgress color='#FC8100' />
-            <Component {...pageProps} />
+      <body>
+        <ThemeProvider theme={theme}>
+          <div className="App">
+            <div className="contenido">
+              <NextNProgress color='#FC8100' />
+              <Component {...pageProps} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </body>
     </>
   )
 }
