2bd28e76262e016e61c3f431afc78eabf5b5239c
Matias Moreno <maatiigmm@gmail.com>
2024-09-10 12:19:54 -0300

    Se agregan botones para seguro soap y de viajes

--- pages/HomePage/Body.js
+++ pages/HomePage/Body.js
@@ -5,11 +5,13 @@ import { PorqueElegirElClub } from '../../components/PorqueElegirElClub'
 import { QuienesNosRespaldan } from '../../components/QuienesNosRespaldan';
 import { CarouselBlogs } from '../../components/CarouselBlogs';
 import { ActivaTusBeneficios } from '../../components/ActivaTusBeneficios';
+import { MasSeguros } from '../../components/MasSeguros';
 export default function Body({ benefits, blogs }) {
     return (
         <>
             <HowToQuote />
             <PorqueElegirElClub />
+            <MasSeguros />
             <QuienesNosRespaldan />
             <CarouselBlogs blogs={blogs} />
             <CotizaAhoraConNosotros />
