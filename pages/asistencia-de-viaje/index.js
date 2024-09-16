97e9e654fde5d99f952d140442c32dee11e80838
Matias Moreno <maatiigmm@gmail.com>
2024-09-16 17:09:17 -0300

    Limpieza de componente

--- pages/asistencia-de-viaje/index.js
+++ pages/asistencia-de-viaje/index.js
@@ -1,15 +1,7 @@
 import { NavBar } from '../../components/NavBar';
-import { CotizaAhoraConNosotros } from '../../components/CotizaAhoraConNosotros';
-import Head from 'next/head';
-import { ActivaTusBeneficios } from '../../components/ActivaTusBeneficios';
-import { QuienesNosRespaldan } from '../../components/QuienesNosRespaldan';
-import { CardMarca } from '../../components/CardMarca';
-import { CardMarcaIcon } from '../../components/CardMarcaIcon';
-import { useEffect, useState } from 'react';
 import styles from '../../styles/soap.module.css';
-import { ImportaciaDeElegirnos } from "../../components/ImportaciaDeElegirnos";
 
-function AsistenciaDeViaje({ data, benefits, brands }) {
+function AsistenciaDeViaje({}) {
     return (
         <div className="body">
             <div className={styles.headerQuienesSomosPage}>
@@ -58,46 +50,9 @@ function AsistenciaDeViaje({ data, benefits, brands }) {
                             <span>
                                 Contratar asistencia de viaje es recomendable por varias razones, ya que es una inversión que te brinda protección y asistencia en situaciones imprevistas, permitiéndote viajar con mayor confianza ofrece protección ante eventos que pueden surgir mientras estás fuera de tu país.
                             </span>
-                            {/* <span>
-                                La asistencia en viaje está diseñada específicamente para cubrir eventos inesperados durante un viaje, por lo general, por un período corto.
-                            </span>
-                            <span>
-                                La asistencia en viaje ofrece protección médica solo para emergencias que ocurran mientras estás de viaje. No cubre condiciones preexistentes ni tratamientos rutinarios.
-                            </span> */}
                         </div>
                     </div>
                 </div>
-                {/*  <div id="texto" className={styles.desarrolloNuestraHistoria}>
-                    <h3>¿Por qué asegurar tu auto con SURA?</h3>
-                    <p>
-                        ✔ Porque cuentan con 79 años de experiencia a nivel mundial.
-                    </p>
-                    <p>
-                        ✔ Porque se preocupan de que siempre tengas las mejores condiciones en tu Seguro Automotriz.
-                    </p>
-                    <p>
-                        ✔ Porque están ubicados a lo largo de todo Chile, contando con 16 sucursales.
-                    </p>
-                    <p>
-                        ✔ Porque puedes contratar tu seguro SOAP en su sitio web de forma simple, rápida y segura.
-                    </p>
-                </div> */}
-                {/* <ImportaciaDeElegirnos /> */}
-
-                {/*  <a href="https://www.segurossura.cl/SoapVenta/Cotizador/enviaPaso?rut=76215627&idConvenio=COREDORESSURAMARZO2024" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
-                    <div className={styles.rootContainerCotizaAhoraConNosotros}>
-                        <div className={styles.imgAsistenciaViaje} />
-                        <div className={styles.contentCotizaAhoraConNosotross}>
-                            <h2 className={styles.titleCotizaAhoraConNosotros} style={{ fontWeight: 'normal', color: 'rgb(0, 51, 160)', marginTop: '5px', lineHeight: '40px', maxWidth: '400px' }}>
-                                Contrata tu <b>Asistencia de Viaje</b> aquí
-                            </h2>
-                            <img loading="lazy" src='https://cds-landing.s3.sa-east-1.amazonaws.com/logo_sura_4b190d7fcb.png' width="100" alt="Logo Renta Nacional" style={{ marginTop: '10px' }} />
-                            <div className={styles.bottomCotizaAhoraConNosotros} style={{ backgroundColor: 'rgb(0, 51, 160)' }}>
-                                <span style={{ color: '#FFFFFF', fontWeight: 'normal' }} >Quiero Cotizar mi <b>ASISTENCIA DE VIAJE</b></span>
-                            </div>
-                        </div>
-                    </div>
-                </a> */}
             </>
         </div>
     )
