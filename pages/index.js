66539a24b947a3a235315975305fb1f285e01a7e
diego <diego.gutierrez684@gmail.com>
2025-02-03 20:39:57 -0300

    add whatsap button

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ function Home({ benefits, blogs, data }) {
       <Head>
         <title> Club del Seguro</title>
         <meta name="description" content={data?.data.meta} />
-        <script src="/static/scripts.js" />
+        {/* <script src="/static/scripts.js" /> */}
       </Head>
       <Hidden only={["xs", "sm"]}>
         <Header title={data.data.title} description={data.data.description} />
